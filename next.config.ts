f04fc25846ca8f1d8c8fe9827c784d94e47d6291
Stephen <stephenallstar24@vt.edu>
2025-06-08 15:28:39 -0400

    Publish to sealqa.github.io

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: isProd ? "export" : "standalone", // Export only for production
-  distDir: "docs",                          // GitHub Pages expects this folder
-  trailingSlash: true,                     // Required for static routing
+  output: isProd ? "export" : "standalone",
+  distDir: "docs",
+  trailingSlash: true,
   images: {
-    unoptimized: true                      // Required for export mode (no server-side image optimization)
+    unoptimized: true
   }
 };
 
