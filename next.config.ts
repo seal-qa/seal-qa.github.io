ab4b99a5c52139e20a5fc67b0c85d19fd26d10b5
Stephen <stephenallstar24@vt.edu>
2025-06-08 15:37:50 -0400

    Publish to sealqa.github.io

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
-
 const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
+  /* config options here */
   output: isProd ? "export" : "standalone",
-  distDir: "docs",
-  trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
